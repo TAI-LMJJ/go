849242b15a0a518116db3aa7769cd105e01f6135
Gram Liu <gram.liu226@gmail.com>
2023-04-06 02:27:20 -0400

    Add redirect for home page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "https://t-ai.app",
+        permanent: false,
+      },
+    ];
+  },
   async rewrites() {
     return [
       {
